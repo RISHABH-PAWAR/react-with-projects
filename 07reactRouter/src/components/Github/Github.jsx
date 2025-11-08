dbe8b670d9e49ab476143d1d672f75f8693b28e9
rishu <rishabhpawar401@gmail.com>
2025-11-09 03:00:17 +0530

    React-Router

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+export default function Github() {
+
+  const data = useLoaderData()
+
+  // const [data , setData] = useState([])
+
+  // useEffect(()=>{
+  //   fetch('https://api.github.com/users/RISHABH-PAWAR')
+  //   .then(response => response.json())
+  //   .then(data =>{
+  //     console.log(data)
+  //     setData(data)
+  //   })
+  // },[])
+
+
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-1xl'>Github Follower:{data.followers} 
+    <img src={data.avatar_url} alt='Git picture' width={300}/>
+    </div>
+  )
+}
+
+export const githubInfoLoader = async() =>{
+  const response = await fetch('https://api.github.com/users/RISHABH-PAWAR')
+  return response.json()
+}
